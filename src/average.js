8611e41198ae1c331e7a93f2d1f9720f1ffc412d
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-07-27 15:53:35 -0300

    Requisito 01

--- src/average.js
+++ src/average.js
@@ -11,9 +11,31 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
+const assert = require('assert');
 
-const average = () => {
-  // add your implementation here
+const average = (array) => {
+  if (array.length === 0) return undefined;
+ if (typeof array !== 'object') return undefined;
+    let counter = 0;
+    let sum = 0;
+    for (let i = 0; i < array.length; i += 1) {
+      if (typeof array[i] !== 'number') return undefined;
+      sum += array[i];
+      counter += 1;
+    }
+    return Math.round(sum / counter); // Formula de arredondamento de número para o inteiro mais próximo.
 };
 
 module.exports = average;
+
+let parameter = [-1, -2, -3, -4, -5];
+let output = average(parameter);
+assert.strictEqual(typeof average, 'function');
+
+parameter = ['1', '2', '3', '4', '5'];
+output = average(parameter);
+assert.deepStrictEqual(output, undefined);
+
+parameter = [];
+output = average(parameter);
+assert.deepStrictEqual(output, undefined);
